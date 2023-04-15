64d2c70f5748efaaf3d6bc0c32749226d9fa6f76
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-15 14:41:42 -0700

    create basic express server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Testing');
+});
+
+app.listen(PORT, console.log(`Listening at port: ${PORT}...`));
