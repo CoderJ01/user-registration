1dc62605710b904a3c74e49bcb700f4500262204
CoderJ01 <buildingguy@sbcglobal.net>
2023-04-15 18:19:37 -0700

    modify retrieveUser function

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
+const { MongoClient } = require('mongodb');
 const express = require('express');
 const connectDB = require('./MongoDB');
 require('dotenv').config();
+const client = new MongoClient(process.env.MONGO_URI);
 
 const app = express();
 const PORT = 3001;
 
 connectDB();
 
+const retrieveUser = async (email) => {
+    let object;
+
+    await client.connect()
+    const db = client.db(process.env.DB);
+    const cursor = await db.collection('users').findOne({ username: 'monkeySeeMonkeyDo' });
+    object = cursor;
+    
+    setTimeout(() => {
+        client.close();
+    }, 1500)
+
+    console.log(object)
+
+    return object;
+}
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Testing');
+app.get('/', async (req, res) => {
+    retrieveUser('');
+    res.send('Hello World');
 });
 
 app.listen(PORT, console.log(`Listening at port: ${PORT}...`));
+
+module.exports = app;
