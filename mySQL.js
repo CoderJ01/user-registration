c4f8d884cf343fdf23fef136044f7928ea5c0ebc
CoderJ01 <buildingguy@sbcglobal.net>
2023-05-19 18:16:48 -0700

    alter config for db connection

--- mySQL.js
+++ mySQL.js
@@ -1,5 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.CLEAR_DB_DATABASE);
+const sequelize = new Sequelize(
+    process.env.CLEAR_DB_DATABASE, 
+    process.env.CLEAR_DB_USERNAME, 
+    process.env.CLEAR_DB_PASSWORD, 
+    {
+        host: process.env.CLEAR_DB_HOST,
+        dialect: 'mysql'
+    }
+);
 
 module.exports = sequelize;
